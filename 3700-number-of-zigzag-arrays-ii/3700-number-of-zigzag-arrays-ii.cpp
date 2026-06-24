51e64a4ca59545575676f989a51500925c71e6e6
Infinity0075 <162988875+Infinity0075@users.noreply.github.com>
2026-06-24 23:53:41 +0530

    Time: 99 ms (100%), Space: 8.2 MB (100%) - LeetHub

--- 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
@@ -1,67 +1,50 @@
 class Solution {
-    const int MOD = 1e9 + 7;
-    using Matrix = vector<vector<long long>>;
-
-    Matrix multiply(const Matrix& A, const Matrix& B) {
-        int sz = A.size();
-        Matrix C(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < sz; i++) {
-            for (int k = 0; k < sz; k++) {
-                if (!A[i][k]) continue;
-                for (int j = 0; j < sz; j++) {
-                    C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
-                }
-            }
-        }
-
-        return C;
-    }
-
+    static constexpr int p = 1e9 + 7;
 public:
     int zigZagArrays(int n, int l, int r) {
-        if (n == 1) return r - l + 1;
-
         int k = r - l + 1;
-        int sz = 2 * k;
-
-        Matrix M(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < i; j++) {
-                M[i][k + j] = 1;
-            }
-
-            for (int j = i + 1; j < k; j++) {
-                M[k + i][j] = 1;
+        unsigned mat[k][k];
+        memset(mat, 0, sizeof(mat));
+        // init mat
+        for(int i = 0; i < k; ++i) {
+            for(int j = 0; j < i; ++j) {
+                mat[k - j - 1][i] = 1;
             }
         }
-
-        Matrix res(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < sz; i++) {
-            res[i][i] = 1;
+        int pow = n - 1;
+        unsigned res[k];
+        for(int i = 0; i < k; ++i) {
+            res[i] = 1;
         }
-
-        long long p = n - 1;
-
-        while (p > 0) {
-            if (p % 2 == 1) {
-                res = multiply(res, M);
+        while(pow > 0) {
+            if(pow & 1) {
+                // res = res * curr;
+                unsigned tmp[k];
+                memset(tmp, 0, sizeof(tmp));
+                for(int j = 0; j < k; ++j) {
+                    for(int l = 0; l < k; ++l) {
+                        tmp[j] = (tmp[j] + 1ll * res[l] * mat[l][j]) % p;
+                    }
+                }
+                memcpy(res, tmp, sizeof(tmp));
             }
-
-            M = multiply(M, M);
-            p /= 2;
-        }
-
-        long long ans = 0;
-
-        for (int i = 0; i < sz; i++) {
-            for (int j = 0; j < sz; j++) {
-                ans = (ans + res[i][j]) % MOD;
+            // curr = curr * curr;
+            unsigned tmp[k][k];
+            memset(tmp, 0, sizeof(tmp));
+            for(int i = 0; i < k; ++i) {
+                for(int j = 0; j < k; ++j) {
+                    for(int l = 0; l < k; ++l) {
+                        tmp[i][j] = (tmp[i][j] + 1ll * mat[i][l] * mat[l][j]) % p;
+                    }
+                }
             }
+            memcpy(mat, tmp, sizeof(tmp));
+            pow >>= 1;
         }
-
-        return (int)ans;
+        unsigned ret = 0;
+        for(int i = 0; i < k; ++i) {
+            ret = (ret + res[i]) % p;
+        }
+        return 2 * ret % p;
     }
 };
